abd706b0e17410c961e3bb3ce95e28cd6636d41c
Angule Mathias <angulemathias3@gmail.com>
2018-09-01 20:52:36 +0300

    [FEATURE #160215403] Test Post Answer without body

--- tests/test_post_answers.py
+++ tests/test_post_answers.py
@@ -11,22 +11,24 @@ class TestAnswer(unittest.TestCase):
 
     def test_post(self):
         """ Test post answer """
-        response = self.app.post("/api/v1/questions",
+        response = self.app.post("/api/v1/questions/1/answer",
                                  content_type='application/json',
-                                 data=json.dumps(dict(question="This is my question 1"), ))
-        response2 = self.app.post("/api/v1/questions/1/answer",
-                                  content_type='application/json',
-                                  data=json.dumps(dict(answer="This is my answer 1"), ))
-        reply = json.loads(response2.data)
+                                 data=json.dumps(dict(answer="This is my answer 1"), ))
+        reply = json.loads(response.data)
         self.assertEquals(reply["message"], "Answer successfully posted to question")
-        self.assertEquals(response2.status_code, 201)
+        self.assertEquals(response.status_code, 201)
 
     def test_empty_answer_post(self):
         """ Test for posting an empty answer post"""
-        response = self.app.post("/api/v1/questions",
-            content_type='application/json',
-            data=json.dumps(dict(question="This is my question 1"),))
-
+        response = self.app.post("/api/v1/questions/1/answer",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(answer="  "), ))
+        reply = json.loads(response.data)
+        self.assertEquals(reply["message"], "No input given")
+        self.assertEquals(response.status_code, 400)
         response2 = self.app.post("/api/v1/questions/1/answer",
-            content_type='application/json',
-            data=json.dumps(dict(answer="  "),))
+                                  content_type='application/json',
+                                  data=json.dumps(dict(answer="This is"), ))
+        reply = json.loads(response2.data)
+        self.assertEquals(reply["message"], "Input has to be at least 10 characters long")
+        self.assertEquals(response2.status_code, 400)

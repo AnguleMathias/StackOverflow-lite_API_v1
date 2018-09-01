db9d6fc2f04b1b9186256f8e34a65437bd346d72
Angule Mathias <angulemathias3@gmail.com>
2018-09-01 17:14:53 +0300

    [CHORE #160215439]POST existing question test case

--- tests/test_post_question.py
+++ tests/test_post_question.py
@@ -26,3 +26,15 @@ class TestQuestions(unittest.TestCase):
         reply = json.loads(response.data)
         self.assertEquals(reply["message"], "Input has to be at least 10 characters long")
         self.assertEquals(response.status_code, 400)
+
+    def existing_question(self):
+        """ Test for posting question successfully """
+        response = self.app.post("/api/v1/questions",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(question="This is question three four"), ))
+        response2 = self.app.post("/api/v1/questions",
+                                  content_type='application/json',
+                                  data=json.dumps(dict(question="This is question three four"), ))
+        reply = json.loads(response2.data)
+        self.assertEquals(reply["message"], "Question already exists")
+        self.assertEquals(response.status_code, 409)

4694ad00bbe1953a2725ccf771dc355afc2d4522
Angule Mathias <angulemathias3@gmail.com>
2018-09-01 19:02:01 +0300

    [CHORE #160215439] Test to get a specific question when given a wrong id

--- tests/test_get_questions.py
+++ tests/test_get_questions.py
@@ -10,7 +10,7 @@ class TestViewingQuestions(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_fetching_available_questions(self):
+    def test_get_questions(self):
         """Test to get all questions"""
         response = self.app.post("/api/v1/questions",
                                  content_type='application/json',
@@ -38,3 +38,19 @@ class TestViewingQuestions(unittest.TestCase):
                                  data=reply)
         reply2 = json.loads(response2.data.decode())
         self.assertEquals(response2.status_code, 200)
+
+    def test_get_question_with_wrong_id(self):
+        """Test to get a single question with wrong id"""
+        response = self.app.post("/api/v1/questions",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(question="This is my question 1"), ))
+        response = self.app.post("/api/v1/questions",
+                                 content_type='application/json',
+                                 data=json.dumps(dict(question="This is my question 2"), ))
+        reply = json.loads(response.data.decode())
+        response2 = self.app.get("/api/v1/questions/q",
+                                 content_type='application/json',
+                                 data=reply)
+        reply2 = json.loads(response2.data.decode())
+        self.assertEquals(reply2["message"], "Id should be an integer")
+        self.assertEquals(response2.status_code, 400)

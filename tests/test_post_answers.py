d3945f6359ecfa97e5aa18dd523171efccdeb012
Angule Mathias <angulemathias3@gmail.com>
2018-09-01 20:48:35 +0300

    [FEATURE #160215403] Test Post Answer without body

--- tests/test_post_answers.py
+++ tests/test_post_answers.py
@@ -20,3 +20,13 @@ class TestAnswer(unittest.TestCase):
         reply = json.loads(response2.data)
         self.assertEquals(reply["message"], "Answer successfully posted to question")
         self.assertEquals(response2.status_code, 201)
+
+    def test_empty_answer_post(self):
+        """ Test for posting an empty answer post"""
+        response = self.app.post("/api/v1/questions",
+            content_type='application/json',
+            data=json.dumps(dict(question="This is my question 1"),))
+
+        response2 = self.app.post("/api/v1/questions/1/answer",
+            content_type='application/json',
+            data=json.dumps(dict(answer="  "),))

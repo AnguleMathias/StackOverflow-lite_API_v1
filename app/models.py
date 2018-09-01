b5d1fb367fc52a0b8845559d72a8524952d03027
Angule Mathias <angulemathias3@gmail.com>
2018-09-01 19:47:52 +0300

    [FEATURE #160215403] Model class for answers

--- app/models.py
+++ app/models.py
@@ -2,9 +2,26 @@ class Question:
     """
     Model class for the question
     """
+
     def __init__(self, question_id, question):
         self.question_id = question_id
         self.question = question
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+
+class Answer:
+    """
+    Model class for answers
+    """
+    def __init__(self, answer_id, answer, question_id):
+        self.answer_id = answer_id
+        self.answer = answer
+        self.question_id = question_id
 
     def __getitem__(self, item):
         return getattr(self, item)
